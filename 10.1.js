0a39a3f7fa13726ea9f0f35d373a6c035ac4a110
brurya-e <brurya.zak@gmail.com>
2021-11-14 13:55:42 +0200

    10

---
+++ 10.1.js
@@ -0,0 +1,59 @@
+// 1. Write a a function called ‘isString’ that receives 2
+// arguments, a string and a callback function.
+// The function checks that the string is indeed a string.
+// If the string is a string, pass the string to a callback
+// function which logs that string to the console.
+// 2. Create a function called ‘firstWordUpperCase’ that
+// receives 2 arguments, a string and a callback function.
+// The function will capitalize the first word in the sentence
+// and pass the string to a callback function which will create
+// dashes between the words.
+// 3. Call the ‘firstWordUpperCase’ function with a callback of
+// your choice.
+// 4. Create your own function that will receive from one of its
+// arguments a callback function.
+
+//1.
+
+const print = str => {console.log(str)}
+const isString = (str, func)=>{
+    if (typeof str === 'string' || str instanceof String){
+        func(str);
+    }
+}
+isString('srting', print);
+isString(10.5, print);
+
+//2.
+const dashes = str =>{
+    let temp = str.replace(' ', '-');
+    return temp;    
+}
+const firstWordUpperCase = (str, func)=>{
+    let i=0;
+    let temp = [];
+    while(i<str.length && str[i]!= ' ')
+        temp+=str[i++].toUpperCase();
+    temp += str.slice(i);
+    temp = func(temp);    
+    // temp+= '-'+ func(str.slice(i+1)); 
+    return temp;  
+}
+console.log(firstWordUpperCase('fgfg abnb bnb', dashes ));
+
+//3
+console.log(firstWordUpperCase('fgfg abnb bnb', print ));
+
+//4
+
+const add=(a, b)=> a+b;
+const power2Byadd = n => {
+    let sum =0;
+    for(let i =0; i<n; i++)
+        sum+=add(0,n);
+    return sum;    
+}
+console.log(add(2,3));
+console.log(power2Byadd(3));
+console.log(power2Byadd(5));
+
