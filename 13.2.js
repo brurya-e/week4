3549d5c71ef02bd077b631fb99f1fdc095028777
brurya-e <brurya.zak@gmail.com>
2021-11-16 13:12:30 +0200

    13.2

--- 13.2.js
+++ 13.2.js
@@ -2,12 +2,16 @@ const foods = ["falafel", "sabich", "hummus", "pizza with extrapineapple"];
 // a.1. Sort the array of strings from descending to ascending
 // order
 const desc = foods.sort();
-// console.log (desc);
+console.log(desc);
 
 // a.2. Sort the array of strings from ascending to descending
 // order.
 const aesc = (foods.sort()).reverse();
-// console.log (aesc);
+console.log(aesc);
+
+const aesc_1 = foods.sort((a, b) => (a < b ? 1 : -1));
+console.log(aesc_1);
+
 
 const foodsWithUpperCase = [
     "falafel",
@@ -27,20 +31,17 @@ console.log(descB);
 
 // b.2. Sort the array of strings from ascending to descending
 // order
-const aescB = foodsWithUpperCase.sort((a, b) => {
-    let x = a.toLowerCase();
-    let y = b.toLowerCase();
-    return x == y ? 0 : y > x ? 1 : -1;
-});
+const aescB = foodsWithUpperCase.sort((a, b) => ((a.toLowerCase() < b.toLowerCase()) ? 1 : -1))
 console.log(aescB);
 
 const words = ["apple", "supercalifragilisticexpialidocious",
     "hi", "zoo"];
 // c.1. Sort the array of strings from the longest word to the
 // shortest word only using the sort function
-const lengthSort = words.sort((a, b) => {
-    let x = a.length;
-    let y = b.length;
-    return x - y;
-})
+const lengthSort = words.sort((a, b) => (a.length - b.length))
+//  {
+//     let x = a.length;
+//     let y = b.length;
+//     return x - y;
+// })
 console.log(lengthSort);
